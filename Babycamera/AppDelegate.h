cebc12e2c00b8cf111fccecd79b69babec4d02d4
莫景涛 <13677785116@163.com>
2015-01-22 14:17:08 +0800

    Initial Commit

---
+++ Babycamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Babycamera
+//
+//  Created by bear on 15/1/22.
+//  Copyright (c) 2015年 莫景涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Babycamera/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Babycamera
+//
+//  Created by bear on 15/1/22.
+//  Copyright (c) 2015年 莫景涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
